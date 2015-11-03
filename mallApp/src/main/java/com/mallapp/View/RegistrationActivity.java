911dfe3939466de34818e4841e74f9c73354d2ef
Sharjeelh <sjh@quantumcph.com>
2015-11-03 15:12:34 +0500

    splash changes and added registration screen

---
+++ mallApp/src/main/java/com/mallapp/View/RegistrationActivity.java
@@ -0,0 +1,14 @@
+package com.mallapp.View;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RegistrationActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+    }
+
+}
